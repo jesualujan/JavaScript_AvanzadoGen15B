9c008cc3a01a63a824d743add199830aacf04b29
Jesua Luján <code.masterqro@gmail.com>
2022-06-07 20:04:19 -0500

    creamos una función para traer los pokemons por su nombre y ver el tipo

---
+++ 03.httpMethods/pokeapi.js
@@ -0,0 +1,16 @@
+// VAMOS A TRABAJAR LA POKEAPI: https://pokeapi.co/
+// ENDPOINT: https://pokeapi.co/api/v2/pokemon
+
+//* 1 VAMOS  A TRAER A REQUEST QUE ACABAMOS DE INSTALAR
+const request = require('request')
+
+//*2 VAMOS A DECLARAR NUESTRA URI DE MI API
+const URI = 'https://pokeapi.co/api/v2/pokemon'
+
+//*3 VAMOS A CREAR UNA FUNCIÓN QUE PIDA UN POKEMON Y ME DEVUELVA SUS TIPOS.
+
+function getPokemonByName(name){
+    // ES BUENA PRACTICA REVISAR SI LA API ES SENSIBLE A MAYUSCULAS O MINUSCULAS
+    //LA POKEAPI NO LO ES, PODRIAMOS USAR UN LOWERCASE PARA EL NOMBRE 
+}
+
