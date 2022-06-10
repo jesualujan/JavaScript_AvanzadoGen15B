b1cc11bba64c416280f273b039fcae15361d3a93
Roberto <robrenedev@gmail.com>
2022-06-09 19:12:06 -0600

    NASA

--- 04Peticiones/index.js
+++ 04Peticiones/index.js
@@ -1,6 +1,7 @@
 console.log("Este es un ejemplo que consume una API de la NASA usando FETCH")
-/*import fetch from "node-fetch"
 const llave = "B4M0OPihTC0U7OFZcfj6PTizGQlDsl21351VamA7"
+/*import fetch from "node-fetch"
+
 
 var endpoint = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-03-10&end_date=2018-03-16&api_key=${llave}`
 
@@ -47,17 +48,72 @@ for (let i =0;i<listaids.length;i++){
 }*/
 
 
-document.getElementById("fotodiv").innerHTML = `
+/*document.getElementById("fotodiv").innerHTML = `
 	<h1>Ya se conecto el html con el javascript<h1>
 
-` 
+` */
+
+var mars = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=${llave}`
+
+const ejemploMars = async(rover)=>{
+	const informacion = await fetch(mars)
+	var info =informacion.json()
+	
+	console.log(info)
+
+}
+var roverElegido ="curiosity"
+var camaraElegida = "fhaz"
+var dsv = 100
+function seleccion(){
+	let seleccionOpciones = document.getElementById("opcionesSeleccion")
+	roverElegido = seleccionOpciones.value
+	console.log(roverElegido)
+}
+
+function seleccion2(){
+	let seleccionOpciones = document.getElementById("opcionesSeleccion2")
+	camaraElegida = seleccionOpciones.value
+	console.log(camaraElegida)
+	
 
 
 
+}
+
+function ejecutar(){
+	console.log("Se esta haciendo el fetch")
+	var ds =document.getElementById("diasolar")
+	dsv = ds.value
+	
+	marte(roverElegido,camaraElegida,dsv)
+}
+const marte =async(rover,camara,dsv)=>{
+	console.log(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover.toLowerCase()}/photos?sol=${dsv}&camera=${camara.toLowerCase()}&api_key=${llave}`)
+	var respuestaFotos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover.toLowerCase()}/photos?sol=${dsv}&camera=${camara.toLowerCase()}&api_key=${llave}`)
+	var {photos: fotosMarte} = await respuestaFotos.json()
+	var divmarte = document.getElementById("tarjeta")
+	while (divmarte.firstChild){
+		divmarte.removeChild(divmarte.firstChild)
+	}
+	fotosMarte.forEach((foto)=>{
+		let iddiv = "hijo"+String(foto.id)
+		document.getElementById("tarjeta").innerHTML += `
+		<div class="relative border rounded shadow" id=${iddiv}>
+			<img src=${foto.img_src} alt=${foto.id} class="rounded" />
+			<div class="bg-white opacity-70 p-5 absolute bottom-0 text-black font-bold flex flex-col">
+				<span>${foto.earth_date}</span>
+				<span>${foto.camera.name}</span>
+			</div>
+		</div>
+		
+		
+		
+		`
 
 
+	})
 
-
-
+}
 
 
