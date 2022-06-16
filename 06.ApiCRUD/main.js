30d920db922c346c21e4146406fdda104ae5288f
Jesua Luján <code.masterqro@gmail.com>
2022-06-15 19:48:16 -0500

    ejecutamos createAuthor y le pasamos un json con la data

--- 06.ApiCRUD/main.js
+++ 06.ApiCRUD/main.js
@@ -5,4 +5,16 @@ const goodReadsCrud = require ('./Authors.js')
 // goodReadsCrud.listAuthors()
 
 //* LISTA DE UN AUTOR POR SU ID 
-goodReadsCrud.getAuthor(14567)
+goodReadsCrud.getAuthor(14737)
+
+//* CREAMOS UN AUTOR
+// const jsonSend = {
+// 	name: "Ana",
+// 	last_name: "Fernanda",
+// 	nacionalidad: "MX",
+// 	biography: "programmer",
+// 	gender: "F",
+// 	age: 25,
+// 	is_alive: true
+// }
+// goodReadsCrud.createAuthor(jsonSend)
